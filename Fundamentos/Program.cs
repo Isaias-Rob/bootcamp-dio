450b33af07a2b517afc4a178924bcadf760c0eef
Isaias-Rob <isaias.roberto.2008@gmail.com>
2022-10-24 19:45:50 -0300

    Comitando fim fundamentos

--- Fundamentos/Program.cs
+++ Fundamentos/Program.cs
@@ -6,3 +6,10 @@ pessoa.Nome = "Isaias";
 pessoa.Idade = 22;
 
 pessoa.Apresentar();
+
+DateTime dataAtual = DateTime.Now; //pega a data da maquina 
+System.Console.WriteLine(dataAtual);
+DateTime dataSoma = DateTime.Now.AddDays(5);
+System.Console.WriteLine(dataSoma);
+DateTime dataFormatada = DateTime.Now;
+System.Console.WriteLine(dataFormatada.ToString("dd/MM/yyyy HH:mm"));
